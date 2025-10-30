7bf08b8b2f76c216473ef72e7a09ec6bb83dc1c4
Aydhina <reynaaydhina11@gmail.com>
2025-10-30 20:32:25 +0700

    cek

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const projectRoutes = require("./routes/projectRoutes");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 // koneksi ke DB
